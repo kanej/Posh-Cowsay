cf3f72ed69e10ca38173d9ec97dd87a72801260a
kanej <john.kane84@gmail.com>
2012-06-21 22:46:05 +0100

    The cow message is now configurable.

--- cowsay.psm1
+++ cowsay.psm1
@@ -1,8 +1,8 @@
 
-function cowsay() {
-  Write-Host " -----"
-  Write-Host "< Moo >"
-  Write-Host " -----"
+function cowsay($message) {
+  Write-Host " ".padRight($message.length + 3, '-')
+  Write-Host "< $message >"
+  Write-Host " ".padRight($message.length + 3, '-')
   Write-Host "      \  ^__^"
   Write-Host "       \ (00)\________"
   Write-Host "         (__)\        )\/\"
