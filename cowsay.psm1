9264528a00a0729622bd29fc4e0270ff8c9ee0ef
kanej <john.kane84@gmail.com>
2012-06-21 23:22:01 +0100

    Crude support for two line messages.

--- cowsay.psm1
+++ cowsay.psm1
@@ -1,8 +1,26 @@
 
+# Max Width of the Speech Bubble
+$bubbleWidth = 40
+
+function print-messagebubble($message) {
+  if($message.length -lt $bubbleWidth + 1) {
+    Write-Host " ".padRight($message.length + 3, '-')
+    Write-Host "< $message >"
+    Write-Host " ".padRight($message.length + 3, '-')
+  } elseif($message.length -lt (2 * $bubbleWidth) + 1) {
+    $first = $message.substring(0, $bubbleWidth)
+    $second = $message.substring($bubbleWidth).padRight($bubbleWidth, ' ')
+    Write-Host " ".padRight($bubbleWidth + 3, '-')
+    Write-Host "/ $first \"
+    Write-Host "\ $second /"
+    Write-Host " ".padRight($bubbleWidth + 3, '-')
+  } else {
+    throw "The cow is dead I am afraid."
+  }
+}
+
 function cowsay($message) {
-  Write-Host " ".padRight($message.length + 3, '-')
-  Write-Host "< $message >"
-  Write-Host " ".padRight($message.length + 3, '-')
+  print-messagebubble($message) 
   Write-Host "      \  ^__^"
   Write-Host "       \ (00)\________"
   Write-Host "         (__)\        )\/\"
