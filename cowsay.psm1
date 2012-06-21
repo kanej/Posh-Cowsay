1fd489a4312820ba9570e66fe2c39f2f60c2d57e
kanej <john.kane84@gmail.com>
2012-06-21 22:38:29 +0100

    We can now see the cow.

--- cowsay.psm1
+++ cowsay.psm1
@@ -1,6 +1,13 @@
 
 function cowsay() {
-  Write-Host "Moo"
+  Write-Host " -----"
+  Write-Host "< Moo >"
+  Write-Host " -----"
+  Write-Host "      \  ^__^"
+  Write-Host "       \ (00)\________"
+  Write-Host "         (__)\        )\/\"
+  Write-Host "              ||----w |"
+  Write-Host "              ||     ||"
 }
 
 Export-ModuleMember cowsay
