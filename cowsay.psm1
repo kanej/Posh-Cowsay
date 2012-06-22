399399ae7816938c5397c5272fc295aaeea0dd36
kanej <john.kane84@gmail.com>
2012-06-22 08:53:47 +0100

    Pulled out lines function.

--- cowsay.psm1
+++ cowsay.psm1
@@ -2,29 +2,46 @@
 # Max Width of the Speech Bubble
 $bubbleWidth = 40
 
+function convert-message-to-lines($message) {
+  $lines = @()
+  $numberOfLines = [Math]::Ceiling($message.length / $bubbleWidth)
+  
+  if($numberOfLines -eq 1) {
+    $lines += $message
+    return ,$lines
+  }
+  
+  # Get all lines but last
+  0..($numberOfLines - 2) | foreach { $lines += $message.substring(($_ * $bubbleWidth), 40) } 
+  # Append last line
+  $lines += $message.substring(($numberOfLines - 1) * $bubbleWidth).padRight($bubbleWidth) 
+
+  return $lines
+}
+
 function print-messagebubble($message) {
-  if($message.length -lt $bubbleWidth + 1) {
-    Write-Host " ".padRight($message.length + 3, '-')
-    Write-Host "< $message >"
-    Write-Host " ".padRight($message.length + 3, '-')
-  } elseif($message.length -lt (2 * $bubbleWidth) + 1) {
-    $first = $message.substring(0, $bubbleWidth)
-    $second = $message.substring($bubbleWidth).padRight($bubbleWidth, ' ')
+  $lines = convert-message-to-lines($message)
+  if($lines.length -eq 1) {
+    $line = $lines[0]
+    Write-Host " ".padRight($line.length + 3, '-')
+    Write-Host "< $line >"
+    Write-Host " ".padRight($line.length + 3, '-')
+  } elseif($lines.length -eq 2) {
+    $first = $lines[0] 
+    $second = $lines[1]
     Write-Host " ".padRight($bubbleWidth + 3, '-')
     Write-Host "/ $first \"
     Write-Host "\ $second /"
     Write-Host " ".padRight($bubbleWidth + 3, '-')
-  } elseif($message.length -lt (3 * $bubbleWidth) + 1) {
-    $first = $message.substring(0, $bubbleWidth)
-    $second =$message.substring($bubbleWidth, $bubbleWidth)
-    $third = $message.substring(2 * $bubbleWidth).padRight($bubbleWidth, ' ')
+  } else {
+    $first = $lines[0]
+    $second = $lines[1]
+    $last = $lines[$lines.length - 1]
     Write-Host " ".padRight($bubbleWidth + 3, '-')
     Write-Host "/ $first \"
     Write-Host "| $second |"
-    Write-Host "\ $third /"
+    Write-Host "\ $last /"
     Write-Host " ".padRight($bubbleWidth + 3, '-')
-  } else {
-    throw "The cow is dead I am afraid."
   }
 }
 
