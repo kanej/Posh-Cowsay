0186838d6971c368deaa263e3fb32c543284b97a
kanej <john.kane84@gmail.com>
2012-06-24 15:22:59 +0100

    Switched to printing to standard out rather than console host.

--- cowsay.psm1
+++ cowsay.psm1
@@ -23,33 +23,33 @@ function print-messagebubble($message) {
   $lines = convert-message-to-lines($message)
   if($lines.length -eq 1) {
     $line = $lines[0]
-    Write-Host " ".padRight($line.length + 3, '-')
-    Write-Host "< $line >"
-    Write-Host " ".padRight($line.length + 3, '-')
+    Write-Output " ".padRight($line.length + 3, '-')
+    Write-Output "< $line >"
+    Write-Output " ".padRight($line.length + 3, '-')
   } elseif($lines.length -eq 2) {
     $first = $lines[0] 
     $second = $lines[1]
-    Write-Host " ".padRight($bubbleWidth + 3, '-')
-    Write-Host "/ $first \"
-    Write-Host "\ $second /"
-    Write-Host " ".padRight($bubbleWidth + 3, '-')
+    Write-Output " ".padRight($bubbleWidth + 3, '-')
+    Write-Output "/ $first \"
+    Write-Output "\ $second /"
+    Write-Output " ".padRight($bubbleWidth + 3, '-')
   } else {
     $first = $lines[0]
     $last = $lines[$lines.length - 1]
-    Write-Host " ".padRight($bubbleWidth + 3, '-')
-    Write-Host "/ $first \"
-    1..($lines.length - 2) | foreach { Write-Host "|" $lines[$_] "|" }
-    Write-Host "\ $last /"
-    Write-Host " ".padRight($bubbleWidth + 3, '-')
+    Write-Output " ".padRight($bubbleWidth + 3, '-')
+    Write-Output "/ $first \"
+    1..($lines.length - 2) | foreach { Write-Output "|" $lines[$_] "|" }
+    Write-Output "\ $last /"
+    Write-Output " ".padRight($bubbleWidth + 3, '-')
   }
 }
 
 function print-cow() {
-  Write-Host "      \  ^__^             "
-  Write-Host "       \ (00)\________    "
-  Write-Host "         (__)\        )\/\"
-  Write-Host "              ||----w |   "
-  Write-Host "              ||     ||   "
+  Write-Output "      \  ^__^             "
+  Write-Output "       \ (00)\________    "
+  Write-Output "         (__)\        )\/\"
+  Write-Output "              ||----w |   "
+  Write-Output "              ||     ||   "
 }
 
 function cowsay($message) {
